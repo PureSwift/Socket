8c2879a05cd2c107f1fb240f91ce9301046badd4
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-04-02 05:00:21 -0700

    Working on `SocketManager`

--- Sources/Socket/SocketManager.swift
+++ Sources/Socket/SocketManager.swift
@@ -7,40 +7,63 @@
 
 import Foundation
 import SystemPackage
+import Atomics
 
-internal actor SocketManager {
+/// Socket Manager
+internal final class SocketManager {
     
     static let shared = SocketManager()
     
     private var didSetup = false
     
-    private var sockets = [ObjectIdentifier: Unmanaged<Socket>]()
-        
+    private var sockets = [SocketState]()
+    
     private var runloop: CFRunLoop?
     
     private var source: CFRunLoopSource?
+            
+    private var lock = NSLock()
     
     deinit {
         removeRunloop()
     }
     
     private init() {
-        addRunloop()
+        // Add to runloop of background thread from concurrency thread pool
+        Task(priority: .high) { [unowned self] in
+            self.addRunloop()
+        }
     }
     
     func add(_ socket: Socket) {
-        lock.lock()
-        // pass as unowned reference
         let id = ObjectIdentifier(socket)
-        sockets[id] = Unmanaged.passUnretained(socket)
+        let state = SocketState(
+            id: id,
+            fileDescriptor: socket.fileDescriptor,
+            socket: .passUnretained(socket)
+        )
+        // append lock
+        lock.lock()
+        sockets.append(state)
         lock.unlock()
     }
     
     func remove(_ socket: Socket) {
-        lock.lock()
         let id = ObjectIdentifier(socket)
-        sockets[id] = nil
-        lock.unlock()
+        lock.lock()
+        defer { lock.unlock() }
+        guard let index = sockets.firstIndex(where: { $0.id == id }) else {
+            assertionFailure()
+            return
+        }
+        sockets.remove(at: index)
+        
+    }
+    
+    func queueWrite(_ data: Data, for socket: Socket) {
+        lock.lock()
+        defer { lock.unlock() }
+        
     }
     
     /// Setup runloop  once
@@ -51,8 +74,9 @@ internal actor SocketManager {
         context.info = Unmanaged<SocketManager>
             .passUnretained(self)
             .toOpaque()
+        
         let source = CFRunLoopSourceCreate(nil, 0, &context)
-        let runloop = CFRunLoopGetMain()
+        let runloop = CFRunLoopGetCurrent()
         CFRunLoopAddSource(runloop, source, .defaultMode)
         self.source = source
         self.runloop = runloop
@@ -65,13 +89,9 @@ internal actor SocketManager {
     }
     
     internal func poll() {
-        lock.lock()
-        defer { lock.unlock() }
-        
         var pollDescriptors = sockets
             .lazy
-            .sorted(by: { $0.key < $1.key })
-            .map { $0.value.takeUnretainedValue().fileDescriptor }
+            .map { $0.socket.takeUnretainedValue().fileDescriptor }
             .map { FileDescriptor.Poll(fileDescriptor: $0, events: .socket) }
         
         do {
@@ -85,14 +105,27 @@ internal actor SocketManager {
         for poll in pollDescriptors {
             guard poll.returnedEvents.isEmpty == false else { return }
             sockets
-                .values
                 .lazy
                 .map { $0.takeUnretainedValue() }
                 .filter { $0.fileDescriptor == poll.fileDescriptor }
-                .forEach { socket in
-                    socket.didPoll(poll.returnedEvents)
-                }
+                .forEach { $0.didPoll(poll.returnedEvents) }
         }
+    }
+}
+
+// MARK: - Supporting Types
+
+extension SocketManager {
+    
+    struct SocketState {
+        
+        let id: ObjectIdentifier
+        
+        let fileDescriptor: FileDescriptor
+        
+        let socket: Unmanaged<Socket>
+        
+        var hasPendingWrites = false
     }
 }
 
@@ -100,6 +133,7 @@ internal actor SocketManager {
 
 @_silgen_name("swift_socket_manager_perform")
 internal func SocketManagerPerform(_ pointer: UnsafeMutableRawPointer?) {
+    assert(Thread.isMainThread)
     guard let pointer = pointer else {
         assertionFailure()
         return
