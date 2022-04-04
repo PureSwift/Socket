8fd1f8bee53b58cf3f6c0ccce363f687529d0fda
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-04-03 20:18:19 -0700

    Updated logging

--- Sources/Socket/Log.swift
+++ Sources/Socket/Log.swift
@@ -10,6 +10,6 @@ import Foundation
 // Socket logging
 internal func log(_ message: String) {
     if ProcessInfo.processInfo.environment["SWIFTSOCKETDEBUG"] == "1" {
-        NSLog(message)
+        NSLog("Socket: " + message)
     }
 }
