c3960623557db72a6cf7965a172856609f31e696
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-06-08 12:07:23 -0700

    Added `GenericSocketOption.reuseAddress`

--- Sources/Socket/System/SocketOptionID.swift
+++ Sources/Socket/System/SocketOptionID.swift
@@ -29,9 +29,28 @@ public struct GenericSocketOption: RawRepresentable, Equatable, Hashable, Socket
 
 public extension GenericSocketOption {
     
+    /// Enable socket debugging.
     @_alwaysEmitIntoClient
     static var debug: GenericSocketOption { GenericSocketOption(_SO_DEBUG) }
     
+    /// Enable sending of keep-alive messages on connection-oriented sockets.
+    ///
+    /// Expects an integer boolean flag.
     @_alwaysEmitIntoClient
     static var keepAlive: GenericSocketOption { GenericSocketOption(_SO_KEEPALIVE) }
+    
+    /**
+     Allow reuse of local addresses when binding,
+     
+     Indicates that the rules used in validating addresses
+     supplied in a  ``SocketDescriptor.bind(_:retryOnInterrupt:)`` call should allow reuse of local
+     addresses.  For ``IPv4Protocol`` sockets this means that a socket
+     may bind, except when there is an active listening socket
+     bound to the address.  When the listening socket is bound
+     to ``IPv4Address.any`` with a specific port then it is not possible
+     to bind to this port for any local address.  Argument is
+     an integer boolean flag.
+     */
+    @_alwaysEmitIntoClient
+    static var reuseAddress: GenericSocketOption { GenericSocketOption(_SO_REUSEADDR) }
 }
