b108e046ec47310330c866d448520baedc87572e
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-14 01:46:59 -0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ var package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/PureSwift/swift-system",
-            branch: "feature/dynamic-lib"
+            url: "https://github.com/apple/swift-system",
+            from: "1.4.0"
         )
     ],
     targets: [
